d950f1d2b1f0401289ebbb87143f9cee222f97ad
reginaldo-basilio <reginaldobasiliocunha@gmail.com>
2021-05-13 18:13:20 -0300

    Criando tela de login

--- app/src/main/java/com/example/compromissos/Activity/MainActivity.java
+++ app/src/main/java/com/example/compromissos/Activity/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.compromissos;
+package com.example.compromissos.Activity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.compromissos.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
