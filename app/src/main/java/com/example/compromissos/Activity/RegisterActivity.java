da58447b8dbf9824b7e99489003a5d8c37f2243e
reginaldo-basilio <reginaldobasiliocunha@gmail.com>
2021-05-15 15:44:43 -0300

    Cadastrando novo usuario (apenas email e senha) e migrando para tela principal ao criar usuario

--- app/src/main/java/com/example/compromissos/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/compromissos/Activity/RegisterActivity.java
@@ -1,16 +1,101 @@
 package com.example.compromissos.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapEditText;
 import com.example.compromissos.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private BootstrapButton btnRegister, btnCancel;
+    private BootstrapEditText edtName, edtEmail, edtPassword, edtConfirmPassword;
+    private RadioButton rbMasc, rbFem;
+    private ImageView imgPictureProfile;
+
+    //instanciando firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //recuperando instancia firebase auth
+        mAuth = FirebaseAuth.getInstance();
+
+        edtName = (BootstrapEditText) findViewById(R.id.edtName);
+        edtEmail = (BootstrapEditText) findViewById(R.id.edtEmail);
+        edtPassword = (BootstrapEditText) findViewById(R.id.edtPassword);
+        edtConfirmPassword = (BootstrapEditText) findViewById(R.id.edtConfirmPassword);
+        btnRegister = (BootstrapButton) findViewById(R.id.btnRegister);
+        btnCancel = (BootstrapButton) findViewById(R.id.btnCancel);
+        rbMasc = (RadioButton) findViewById(R.id.rbMasc);
+        rbFem = (RadioButton) findViewById(R.id.rbFem);
+        imgPictureProfile = (ImageView) findViewById(R.id.imgPictureProfile);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                criarConta(edtEmail.getText().toString(), edtPassword.getText().toString());
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        imgPictureProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    private void criarConta(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            Toast.makeText(RegisterActivity.this, "Usuário cadastrado com sucesso!.",
+                                    Toast.LENGTH_SHORT).show();
+                            abrirMainActivity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Falha ao cadastrar usuário!.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void abrirMainActivity(){
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
