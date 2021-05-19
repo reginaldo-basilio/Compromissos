345c06fb41ab21558cb97c95c539e70f43a99452
reginaldo-basilio <reginaldobasiliocunha@gmail.com>
2021-05-19 06:32:59 -0300

    Criando permissoes... Este topico precisa ser revisado, pois nao pediu permissao ao rodar o app

--- app/src/main/java/com/example/compromissos/Activity/LoginActivity.java
+++ app/src/main/java/com/example/compromissos/Activity/LoginActivity.java
@@ -2,9 +2,13 @@ package com.example.compromissos.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +26,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CAMERA = 10;
+    private static final int PERMISSION_WRITE = 11;
+    private static final int PERMISSION_READ = 12;
 
     private BootstrapButton btnLogin, btnCancel, btnRegister, btnCancelAlert, btnSendEmail;
     private BootstrapEditText edtEmail, edtPassword, edtSendEmail;
@@ -109,6 +117,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        verificarPermissoes();
+
         // Check if user is signed in (non-null) and update UI accordingly.
         currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
@@ -159,5 +170,26 @@ public class LoginActivity extends AppCompatActivity {
 
         dialog.show();
     }
+
+    private void verificarPermissoes(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA) && ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) && ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                /////
+
+            }else{
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, PERMISSION_CAMERA);
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_WRITE);
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_READ);
+            }
+
+        }
+    }
+
 
 }
