70f1dfd503f6e2a8f2d74e199b2703c27954490b
reginaldo-basilio <reginaldobasiliocunha@gmail.com>
2021-06-08 22:28:57 -0300

    Concluindo mascara para a data/hora e outros formatos

--- app/src/main/java/com/example/compromissos/Helper/MaskEditText.java
+++ app/src/main/java/com/example/compromissos/Helper/MaskEditText.java
@@ -14,33 +14,44 @@ public class MaskEditText {
     public static final String FORMAT_HOUR = "##h##m";
     public static final String FORMAT_DATE_HOUR = "##/##/## - ##h##m";
 
-    public static TextWatcher mask(final EditText editText, final String mask){
+    /**
+     * Método que deve ser chamado para realizar a formatação
+     *
+     * @param editText
+     * @param mask
+     * @return
+     */
+    public static TextWatcher mask(final EditText editText, final String mask) {
         return new TextWatcher() {
             boolean isUpdating;
             String old = "";
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            @Override
+            public void afterTextChanged(final Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
             }
 
             @Override
             public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
                 final String str = MaskEditText.unmask(s.toString());
                 String mascara = "";
-                if(isUpdating){
+                if (isUpdating) {
                     old = str;
                     isUpdating = false;
                     return;
                 }
-                int i=0;
-                for(final char m : mask.toCharArray()){
-                    if(m != '#' && str.length() > old.length()){
+                int i = 0;
+                for (final char m : mask.toCharArray()) {
+                    if (m != '#' && str.length() > old.length()) {
                         mascara += m;
                         continue;
                     }
-                    try{
+                    try {
                         mascara += str.charAt(i);
-                    }catch(final Exception e){
+                    } catch (final Exception e) {
                         break;
                     }
                     i++;
@@ -49,17 +60,11 @@ public class MaskEditText {
                 editText.setText(mascara);
                 editText.setSelection(mascara.length());
             }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-
         };
     }
 
-    public static String unmask(final String s){
-        return s.replace("[.]", "").replaceAll("[-]", "").replaceAll("[/]", "").replaceAll("[(]", "").replaceAll("[)]", "").replaceAll("[ ]", "").replaceAll("[h]", "").replaceAll("[m]", "");
+    public static String unmask(final String s) {
+        return s.replaceAll("[.]", "").replaceAll("[-]", "").replaceAll("[/]", "").replaceAll("[(]", "").replaceAll("[ ]", "").replaceAll("[)]", "").replaceAll("[h]", "").replaceAll("[m]", "");
     }
 
 
